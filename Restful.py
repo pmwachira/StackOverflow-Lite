20d9616ce65b5cd7ed910a0b90ea394be2187ff1
mushirih <pmwachira@gmail.com>
2018-08-22 00:40:08 +0300

    21_08

--- Restful.py
+++ Restful.py
@@ -1,7 +1,7 @@
-from flask import(
-    Flask,
-    render_template
-)
+from flask import Flask,render_template,flash,redirect,url_for,sessions,logging,request
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
+
 from data import Questions
 import connexion
 import psycopg2
@@ -20,19 +20,22 @@ def home():
 
 @app.route('/auth/signup')
 
-def add_user(user_name,user_email):
+def add_user(name,email,username,password):
+    conn = psycopg2.connect("host= localhost dbname=StackOverflowLite user=postgres password=postgres")
     try:
 
-        sql="""INSERT INTO users(user_name,_user_email) VALUES (%s,%s) RETURNING user_id"""
+        sql="""INSERT INTO users(user_name,user_email,password) VALUES (%s,%s,%s) RETURNING user_id"""
         cur=conn.cursor()
 
-        cur.execute(sql,(user_name,user_email))
+        cur.execute(sql,(name,email,password))
 
         user_id= cur.fetchone()[0]
 
         conn.commit()
 
         cur.close()
+
+
     except(Exception,psycopg2.DatabaseError) as error:
         print (error)
     finally:
@@ -56,5 +59,32 @@ def question(id):
 # @app.route('/questions/<questionId>/answers')
 # @app.route('/questions/<questionId>/answers/<answerId>')
 
+class RegisterForm(Form):
+    name=StringField('Name',[validators.Length(min=1,max=50)])
+    username=StringField('Username',[validators.Length(min=4,max=25)])
+    email=StringField('Email',[validators.Length(min=6,max=50)])
+    password=PasswordField('Password',[
+        validators.DataRequired(),
+        validators.EqualTo('confirm',message='Passwords do not match')
+    ])
+    confirm=PasswordField('Confirm Password')
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form=RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name=form.name.data
+        email=form.email.data
+        username=form.username.data
+        password=sha256_crypt.encrypt(str(form.password.data))
+
+        show=add_user(name,email,username,password)
+        flash('You are now registered'+str(show), 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html',form=form)
+
+
 if __name__ == '__main__':
+    app.secret_key='my very unsecured secret key'
     app.run(host='0.0.0.0',port=5000,debug=True)
